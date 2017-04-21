eff829a0e3b77d04a1b214a9054793b2e01d36b1
Jaga Apple <jagaapple@brushdown.com>
2017-04-21 19:04:40 +0900

    [Add] Add a template file of Case Methods
    Add a template file of Case Methods, so o not implement yet.

---
+++ lib/string_foundation/case.rb
@@ -0,0 +1,47 @@
+# ==============================================================================
+# LIB - STRING FOUNDATION - CASE
+# ==============================================================================
+# frozen_string_literal: true
+class String
+
+  # Convert to lowerCamelCase.
+  def to_lcamel
+  end
+
+  # Convert to UpperCamelCase.
+  def to_ucamel
+  end
+
+  # Convert to lower_snake_case.
+  def to_lsnake
+  end
+
+  # Convert to Upper_Snake_Case.
+  def to_usnake
+  end
+
+  # Convert to lower-kebab-case.
+  def to_lkebab
+  end
+
+  # Convert to Upper-Kebab-Case.
+  def to_ukebab
+  end
+
+  # Convert to lower space case.
+  def to_lspace
+  end
+
+  # Convert to Upper Space Case.
+  def to_uspace
+  end
+
+  # Convert to lower.dot.case.
+  def to_ldot
+  end
+
+  # Convert to Upper.Dot.Case.
+  def to_udot
+  end
+
+end
