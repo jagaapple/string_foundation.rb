8c3d7807b679b0d8bfeb62119b4f0829a202a78d
Cosmic Phi <cosmicphi@brushdown.com>
2017-04-29 19:22:47 +0900

    [Add] Implement “to_lsnake” and “to_usnake” methods
    Implement methos which convert string to snake case.
    Also some modifications is done.

--- lib/string_foundation/case.rb
+++ lib/string_foundation/case.rb
@@ -7,7 +7,7 @@ class String
   # Convert to lowerCamelCase.
   def to_lcamel
     ucamel = self.to_ucamel
-    ucamel[0].downcase + ucamel[1..-1]
+    ucamel.make_head_lower
   end
 
   # Convert to UpperCamelCase.
@@ -30,24 +30,24 @@ class String
 
   # Convert to lower_snake_case.
   def to_lsnake
+    usnake = self.to_usnake
+    usnake.downcase
   end
 
   # Convert to Upper_Snake_Case.
   def to_usnake
-    if self.include?('_') || self.include?('-') || self.include?('.') || self.include?(' ')
-      str = self
-      str.split('_').map do |uw|
-        uw.split('-').map do |hw|
-          hw.split('.').map do |dw|
-            dw.split(' ').map do |sw|
-              sw.capitalize
+    str = self
+    str.split('_').map do |uw|
+      uw.split('-').map do |hw|
+        hw.split('.').map do |dw|
+          dw.split(' ').map do |sw|
+            sw.split_camel.map do |cw|
+              cw.capitalize
             end.join('_')
           end.join('_')
         end.join('_')
       end.join('_')
-    else
-      self[0].upcase + self[1..-1]
-    end
+    end.join('_')
   end
 
   # Convert to lower-kebab-case.
@@ -72,6 +72,32 @@ class String
 
   # Convert to Upper.Dot.Case.
   def to_udot
+  end
+
+  def split_camel
+    words = []
+    word = []
+    self.each_char do |c|  
+      if c.is_upper?
+        words << word.join if not word.size == 0
+        word = [c]
+      else
+        word << c
+      end
+    end
+    words << word.join
+  end
+
+  def is_upper?
+    /[[:upper:]]/.match(self)
+  end
+
+  def is_lower?
+    /[[:lower:]]/.match(self)
+  end
+
+  def make_head_lower
+    self[0].downcase + self[1..-1]
   end
 
 end
