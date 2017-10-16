ed11f7ab3d1b36745ef3cafb7ba2226d25166c96
Ryuji Akamatsu <seven.television.commercials@gmail.com>
2017-10-17 02:06:15 +0900

    [Add] Implement character length lt gt methods
    - Implement `.lt?` method
    - Implement `.lte?` method
    - Implement `.gt?` method
    - Implement `.gte?` method
    - Add test for above methods

--- lib/string_foundation/length.rb
+++ lib/string_foundation/length.rb
@@ -14,4 +14,40 @@ class String
     self.length.between?(length.first, length.last)
   end
 
+  # Compare characters length is less than specified number.
+  def length_lt?(length)
+    String.allow_only_integer_argument(length)
+
+    self.length < length
+  end
+
+  # Compare characters length is less than equal specified number.
+  def length_lte?(length)
+    String.allow_only_integer_argument(length)
+
+    self.length <= length
+  end
+
+  # Compare characters length is greater than specified number.
+  def length_gt?(length)
+    String.allow_only_integer_argument(length)
+
+    self.length > length
+  end
+
+  # Compare characters length is greater than equal specified number.
+  def length_gte?(length)
+    String.allow_only_integer_argument(length)
+
+    self.length >= length
+  end
+
+  private
+
+  def self.allow_only_integer_argument(argument)
+    unless [Integer, Fixnum, Bignum].include?(argument.class)
+      raise ArgumentError.new("argument must be Integer (including Fixnum or Bignum)")
+    end
+  end
+
 end
