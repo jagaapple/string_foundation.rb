f804458437d42e72abecd35dd973c7606c6ea02e
Ryuji Akamatsu <seven.television.commercials@gmail.com>
2017-10-16 19:00:10 +0900

    [Fix] Fix reviewed code, and improve
    - Use `let` and `subject` in test code
    - Corresponding to ruby 2.1.x to 2.3.x
    - Clean code

--- lib/string_foundation/length.rb
+++ lib/string_foundation/length.rb
@@ -2,15 +2,16 @@
 # LIB - STRING FOUNDATION - LENGTH
 # ==============================================================================
 # frozen_string_literal: true
-
 class String
+
+  # Compare characters length
   def length?(length)
-    unless length.class == Integer || length.class == Range
-      raise ArgumentError.new("argument must be an Integer or a Range")
+    unless [Integer, Range, Fixnum, Bignum].include?(length.class)
+      raise ArgumentError.new("argument must be Integer (it called Fixnum or Bignum in Ruby 2.3 or lower) or Range")
     end
 
-    return self.length == length if length.class == Integer
-
-    self.length.between?(length.first, length.last) if length.class == Range
+    return self.length == length unless length.class == Range
+    self.length.between?(length.first, length.last)
   end
+
 end
--- spec/string_foundation/length_spec.rb
+++ spec/string_foundation/length_spec.rb
@@ -4,32 +4,60 @@
 # frozen_string_literal: true
 describe "[ Length Methods ]" do
 
+  # ----------------------------------------------------------------------------
+  # Compare Characters Length
+  # ----------------------------------------------------------------------------
   describe "COMPARE CHARACTERS LENGTH" do
-    context "when the argument is String class," do
-      it { expect{ "string".length?("6") }.to raise_error ArgumentError }
+    let(:string) { "string" }
+    let(:expected_length) { 0 }
+    subject { string.length?(expected_length) }
+
+    context "when a argument is String class," do
+      let(:expected_length) { "6" }
+
+      it { is_expected_as_block.to raise_error ArgumentError }
     end
 
     context "when characters length and argument are the same," do
-      it { expect("string".length?(6)).to be true }
+      let(:expected_length) { 6 }
+
+      it { is_expected.to be true }
     end
 
     context "when characters length and argument are not the same," do
-      it { expect("string".length?(5)).to be false }
+      let(:expected_length) { 5 }
+
+      it { is_expected.to be false }
     end
 
-    context "when character length are within range of argument," do
-      it {
-        expect("string".length?(1..10)).to be true
-        expect("string".length?(6..10)).to be true
-        expect("string".length?(1..6)).to be true
-      }
+    context "when characters length are within range of argument `1..10`," do
+      let(:expected_length) { 1..10 }
+
+      it { is_expected.to be true }
     end
 
-    context "when character length are out of range of argument," do
-      it {
-        expect("string".length?(1..5)).to be false
-        expect("string".length?(7..10)).to be false
-      }
+    context "when characters length are within range of argument `6..10`," do
+      let(:expected_length) { 6..10 }
+
+      it { is_expected.to be true }
+    end
+
+    context "when characters length are within range of argument `1..6`," do
+      let(:expected_length) { 1..6 }
+
+      it { is_expected.to be true }
+    end
+
+    context "when characters length are out of range of argument `1..5`," do
+      let(:expected_length) { 1..5 }
+
+      it { is_expected.to be false }
+    end
+
+    context "when characters length are out of range of argument `7..10`," do
+      let(:expected_length) { 7..10 }
+
+      it { is_expected.to be false }
     end
   end
 
