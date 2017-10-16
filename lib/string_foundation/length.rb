cf543a245bbbbeeecee7a4c88fc89bd80dde59d3
Ryuji Akamatsu <seven.television.commercials@gmail.com>
2017-10-17 00:12:29 +0900

    [Clean] Refactor reviewed code
    - Improve sentences
    - Improve conditions
    - Improve test code to abstract

--- lib/string_foundation/length.rb
+++ lib/string_foundation/length.rb
@@ -4,13 +4,13 @@
 # frozen_string_literal: true
 class String
 
-  # Compare characters length
+  # Compare characters length.
   def length?(length)
     unless [Integer, Range, Fixnum, Bignum].include?(length.class)
-      raise ArgumentError.new("argument must be Integer (it called Fixnum or Bignum in Ruby 2.3 or lower) or Range")
+      raise ArgumentError.new("argument must be Integer (including Fixnum or Bignum) or Range")
     end
 
-    return self.length == length unless length.class == Range
+    return self.length == length unless length.instance_of?(Range)
     self.length.between?(length.first, length.last)
   end
 
